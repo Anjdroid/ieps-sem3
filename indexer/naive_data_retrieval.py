07f1865ccb58c6445d4d7115d84d60017c433dd0
JanFekonja <jan.fekonja@gmail.com>
2019-05-28 23:20:06 +0200

    Added naive data retrieval without the SQLite database

--- indexer/naive_data_retrieval.py
+++ indexer/naive_data_retrieval.py
@@ -1,8 +1,11 @@
 import os
 from bs4 import BeautifulSoup
 import codecs
+import nltk
+from nltk.corpus import stopwords
 import operator
 import time
+import re
 
 
 def get_html_content(root, name):
@@ -21,14 +24,30 @@ def get_html_content(root, name):
     return '\n'.join(x for x in chunks if x)
 
 
-if __name__ == "__main__":
+"""
+def remove_stopwords(words):
+    punctuation = ['.', ',', '!', '?', '-', '>', '<', ':', ';', ')', '(', '--', '–', '/', "''", '', '|', '...', '``',
+                   '»', '«']
+    for p in punctuation:
+        words = words.replace(p, "")
+
+    sw = set(stopwords.words('slovenian'))
+
+    words_list = words.split(" ")
+    new_word = ""
+    for w in words_list:
+        if w not in sw:
+            new_word = new_word + " " + w
+
+    return new_word
+"""
+
+
+def data_retrieval(query):
     start_time = time.time()
 
-    query = "predelovalne dejavnosti"
-    # query = "trgovina"
-    # query = "social services"
-
-    query_list = (query.lower()).split(" ")
+    new_query = query.replace(" in", "")
+    query_list = (new_query.lower()).split(" ")
     freq = {}
 
     for r, d, f in os.walk("../data/"):
@@ -37,91 +56,124 @@ if __name__ == "__main__":
                 content = (get_html_content(r, file)).lower()
                 counter = 0
                 for x in query_list:
-                    counter = counter + content.count(x)
+                    all_matches = re.findall(r'\b%s\b' % x, content)
+                    counter = len(all_matches)
 
                 if counter > 0:
                     freq[r + "/" + file] = counter
 
     sorted_freq = sorted(freq.items(), key=operator.itemgetter(1), reverse=True)
 
-    print("Results for a query: \"" + query + "\" ")
-    print("")
+    result = ""
 
-    elapsed_time = time.time() - start_time
-    print("Results found in " + str(elapsed_time) + "s.")
-    print("")
-
-    print("Frequencies Document                                  Snippet")
-    print("----------- ----------------------------------------- -----------------------------------------------------------")
     for key, value in sorted_freq:
         path = key[:-(len(str((key.split("/"))[-1])) + 1)]
         filename = (key.split("/"))[-1]
-        print(str(value), end="")
+        result = result + str(value)
         for i in range(len("----------- ") - len(str(value))):
-            print(" ", end="")
-        print(filename, end="")
+            result = result + " "
+        result = result + filename
         for i in range(len("----------------------------------------- ") - len(str(filename))):
-            print(" ", end="")
+            result = result + " "
         content = get_html_content(path, filename)
         content_list = content.split("\n")
 
+        if value > 5:
+            n = 5
+        else:
+            n = value
+
         stevec = 0
-        snippet = ""
         for cl in content_list:
             content_list_list = cl.split(" ")
             used_words = []
-            last_word = 0
             for ql in range(len(query_list)):
                 for cll in range(len(content_list_list)):
-                    if query_list[ql] in (content_list_list[cll]).lower() and cll not in used_words and stevec < 5:
+                    if re.search(r'\b%s\b' % query_list[ql],
+                                 (content_list_list[cll]).lower()) is not None and cll not in used_words and stevec < n:
                         stevec = stevec + 1
                         if cll - 3 == 0:
-                            snippet = snippet + content_list_list[cll - 3] + " " + content_list_list[cll - 2] + " " + \
-                                      content_list_list[cll - 1] + " "
+                            result = result + content_list_list[cll - 3] + " " + content_list_list[cll - 2] + " " + \
+                                     content_list_list[cll - 1] + " "
                         elif cll - 3 > 0:
-                            if len(snippet) >= 4 and "... " == snippet[(len(snippet) - 4) : len(snippet)]:
-                                snippet = snippet + content_list_list[cll - 3] + " " + content_list_list[
+                            if len(result) >= 4 and "... " == result[(len(result) - 4): len(result)]:
+                                result = result + content_list_list[cll - 3] + " " + content_list_list[
                                     cll - 2] + " " + content_list_list[cll - 1] + " "
                             else:
-                                snippet = snippet + "... " + content_list_list[cll - 3] + " " + content_list_list[
+                                result = result + "... " + content_list_list[cll - 3] + " " + content_list_list[
                                     cll - 2] + " " + content_list_list[cll - 1] + " "
                         elif cll - 2 == 0:
-                            snippet = snippet + content_list_list[cll - 2] + " " + content_list_list[cll - 1] + " "
+                            result = result + content_list_list[cll - 2] + " " + content_list_list[cll - 1] + " "
                         elif cll - 1 == 0:
-                            snippet = snippet +content_list_list[cll - 1] + " "
-                        snippet = snippet + content_list_list[cll] + " "
+                            result = result + content_list_list[cll - 1] + " "
+                        result = result + content_list_list[cll] + " "
                         used_words.append(cll)
                         last_word = cll
-                        if ql + 1 < len(query_list) and cll + 1 < len(content_list_list) and query_list[ql + 1] in (
-                                content_list_list[cll + 1]).lower() and cll + 1 not in used_words:
-                            snippet = snippet + content_list_list[cll + 1] + " "
+                        if ql + 1 < len(query_list) and cll + 1 < len(content_list_list) and re.search(
+                                r'\b%s\b' % query_list[ql + 1],
+                                (content_list_list[cll + 1]).lower()) is not None and cll + 1 not in used_words:
+                            result = result + content_list_list[cll + 1] + " "
                             used_words.append(cll + 1)
                             last_word = cll + 1
-                            if ql + 2 < len(query_list) and cll + 2 < len(content_list_list) and query_list[ql + 2] in (
-                                    content_list_list[cll + 2]).lower() and cll + 2 not in used_words:
-                                snippet = snippet + content_list_list[cll + 2] + " "
+                            if ql + 2 < len(query_list) and cll + 2 < len(content_list_list) and re.search(
+                                    r'\b%s\b' % query_list[ql + 2],
+                                    (content_list_list[cll + 2]).lower()) is not None and cll + 2 not in used_words:
+                                result = result + content_list_list[cll + 2] + " "
                                 used_words.append(cll + 2)
                                 last_word = cll + 2
-                                if ql + 3 < len(query_list) and cll + 3 < len(content_list_list) and query_list[
-                                    ql + 3] in (content_list_list[cll + 3]).lower() and cll + 3 not in used_words:
-                                    snippet = snippet + content_list_list[cll + 3] + " "
+                                if ql + 3 < len(query_list) and cll + 3 < len(content_list_list) and re.search(
+                                        r'\b%s\b' % query_list[ql + 3],
+                                        (content_list_list[cll + 3]).lower()) is not None and cll + 3 not in used_words:
+                                    result = result + content_list_list[cll + 3] + " "
                                     used_words.append(cll + 3)
                                     last_word = cll + 3
-                                    if ql + 4 < len(query_list) and cll + 4 < len(content_list_list) and query_list[
-                                        ql + 4] in (content_list_list[cll + 4]).lower() and cll + 4 not in used_words:
-                                        snippet = snippet + content_list_list[cll + 4] + " "
+                                    if ql + 4 < len(query_list) and cll + 4 < len(content_list_list) and re.search(
+                                            r'\b%s\b' % query_list[ql + 4],
+                                            (content_list_list[
+                                                cll + 4]).lower()) is not None and cll + 4 not in used_words:
+                                        result = result + content_list_list[cll + 4] + " "
                                         used_words.append(cll + 4)
                                         last_word = cll + 4
                         if last_word + 1 == len(content_list_list) - 1:
-                            snippet = snippet + content_list_list[last_word + 1] + " "
+                            result = result + content_list_list[last_word + 1] + " "
                         elif last_word + 2 == len(content_list_list) - 1:
-                            snippet = snippet + content_list_list[last_word + 1] + " " + content_list_list[
+                            result = result + content_list_list[last_word + 1] + " " + content_list_list[
                                 last_word + 2] + " "
                         elif last_word + 3 == len(content_list_list) - 1:
-                            snippet = snippet + content_list_list[last_word + 1] + " " + content_list_list[
+                            result = result + content_list_list[last_word + 1] + " " + content_list_list[
                                 last_word + 2] + " " + content_list_list[last_word + 3] + " "
                         elif last_word + 3 < len(content_list_list) - 1:
-                            snippet = snippet + content_list_list[last_word + 1] + " " + content_list_list[
+                            result = result + content_list_list[last_word + 1] + " " + content_list_list[
                                 last_word + 2] + " " + content_list_list[last_word + 3] + " ... "
 
-        print(snippet)
+        result = result + "\n"
+
+    print("Results for a query: \"" + query + "\"\n")
+
+    elapsed_time = time.time() - start_time
+    print("Results found in " + str(round(elapsed_time * 1000)) + "ms.\n")
+
+    print("Frequencies Document                                  Snippet")
+    print(
+        "----------- ----------------------------------------- -----------------------------------------------------------")
+    print(result)
+
+
+if __name__ == "__main__":
+    query = "predelovalne dejavnosti"
+    data_retrieval(query)
+
+    query = "trgovina"
+    data_retrieval(query)
+
+    query = "social services"
+    data_retrieval(query)
+
+    query = "statistični urad RS"
+    data_retrieval(query)
+
+    query = "otroški dodatki in državne štipendije"
+    data_retrieval(query)
+
+    query = "fakulteta"
+    data_retrieval(query)
